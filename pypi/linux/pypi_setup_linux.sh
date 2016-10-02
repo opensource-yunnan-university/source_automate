9e8e27c8259a5b2dea35aa7bea4e39797ab2fdca
Hukuang <thiswind@gmail.com>
2016-10-03 00:00:16 +0800

    Update pypi_setup_linux.sh
    add check if /root/.pip is already exits, then move it to /root/.pip.bak

--- pypi/linux/pypi_setup_linux.sh
+++ pypi/linux/pypi_setup_linux.sh
@@ -4,6 +4,14 @@ mkdir ~/.pip
 cd ~/.pip
 wget https://raw.githubusercontent.com/opensource-yunnan-university/source_automate/master/pypi/linux/pip.conf
 
+sudo su
+if [ -e /root/.pip ]
+then
+  mv /root/.pip /root/.pip.bak
+  echo 'your /root/.pip has been backuped to /root/.pip.bak
+fi
+exit
+
 cd ~
 home=$(pwd)
 sudo ln -s $home/.pip /root/.pip
