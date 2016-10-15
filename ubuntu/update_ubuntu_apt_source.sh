51615ec28c22894094dcce6fecdf1493ce5a6820
Hukuang <thiswind@gmail.com>
2016-10-15 14:23:48 +0800

    Update update_ubuntu_apt_source.sh
    remove 'sudo' for docker use

--- ubuntu/update_ubuntu_apt_source.sh
+++ ubuntu/update_ubuntu_apt_source.sh
@@ -5,18 +5,18 @@ NAME=""
 
 # backup 
 echo "backup /etc/apt/sources.list"
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.$DATE
+cp /etc/apt/sources.list /etc/apt/sources.list.$DATE
 
 # test release name
 echo "check release name"
 # is 14.04?
-if [ -n "$(sudo lsb_release -r | grep 12.04)" ]; then
+if [ -n "$(lsb_release -r | grep 12.04)" ]; then
 	NAME="12.04"
-elif [ -n "$(sudo lsb_release -r | grep 14.04)" ]; then
+elif [ -n "$(lsb_release -r | grep 14.04)" ]; then
 	NAME="14.04"
-elif [ -n "$(sudo lsb_release -r | grep 15.10)" ]; then
+elif [ -n "$(lsb_release -r | grep 15.10)" ]; then
 	NAME="15.10"
-elif [ -n "$(sudo lsb_release -r | grep 16.04)" ]; then
+elif [ -n "$(lsb_release -r | grep 16.04)" ]; then
 	NAME="16.04"
 else
 	echo "can not determine release name, exit"
@@ -31,10 +31,10 @@ echo "download sources.list"
 wget -O ~/sources.list.ynu https://raw.githubusercontent.com/opensource-yunnan-university/source_automate/master/ubuntu/sources.list.ynu.$NAME
 
 echo "overwrite sources.list"
-sudo cp ~/sources.list.ynu /etc/apt/sources.list
+cp ~/sources.list.ynu /etc/apt/sources.list
 
 echo "refresh cache"
-sudo apt-get update
+apt-get update
 
 echo 'setup ok.'
 echo 'Welcome to opensource of Yunnan University.'
